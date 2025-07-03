b45fb6e4efae5f1dffc73a02c583a5b5babc4c61
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2025-07-02 21:54:36 -0300

    feat: :seedling: Adding EmailDto

---
+++ src/main/java/zhant/enviaEmailMicroservice/dto/EmailDto.java
@@ -0,0 +1,28 @@
+package zhant.enviaEmailMicroservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDto {
+
+	@NotBlank
+	private String sender;
+	@NotBlank
+	@Email
+	private String emailFrom;
+	@NotBlank
+	@Email
+	private String emailTo;
+	@NotBlank
+	private String subject;
+	@NotBlank
+	private String text;
+}
